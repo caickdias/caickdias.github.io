3be1ead1e1dbbc19352e4b8dadf243e5ccc0f6ee
caickdias <caickandrade0@gmail.com>
2022-11-10 17:25:51 -0300

    added tattoo highlight on hover

--- src/components/HeroAvatar.jsx
+++ src/components/HeroAvatar.jsx
@@ -8,10 +8,17 @@ const HeroAvatar = () => {
         <div className='absolute bottom-[7rem] top-0 right-32 left-0 m-auto 
         w-96 h-96 
         overflow-hidden transition-all duration-300 
-        rounded-full 
+        rounded-full caick 
         border-b-8 border-blue-700 border-dotted
         '>            
             
+            <button className='absolute -top-6 bottom-0 left-0 -right-4 m-auto 
+              z-30 w-[1.1rem] h-[1.1rem] tattoo 
+              opacity-40'
+              onClick={() => alert('tattoo')}
+            >              
+            </button>
+
             <img className='absolute -bottom-0 top-0 right-0 left-0 m-auto             
             transition-all duration-300' 
             width={300}
