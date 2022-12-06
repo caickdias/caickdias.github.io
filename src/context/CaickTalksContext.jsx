01a23cb515ef6d7ea8abe6b402d5ef95ffbdb381
caickdias <caickandrade0@gmail.com>
2022-12-06 03:20:56 -0300

    Updates

--- src/context/CaickTalksContext.jsx
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-const CaickTalksContext = createContext({});
-
-export default CaickTalksContext;
--- src/utils/functions.js
+++
@@ -1,7 +0,0 @@
-const createMemoryGameDeck = cards => {
-    return [...cards, ...cards].sort(() => Math.random() - 0.5);
-}
-
-export {
-    createMemoryGameDeck,
-}
